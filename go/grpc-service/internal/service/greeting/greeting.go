2ed176a4621be1abcbf7d5dc3df2d8ff5c9d9428
Milad Irannejad <moorara@users.noreply.github.com>
2021-11-07 15:47:16 -0500

    Rename templates: drop vertical

---
+++ go/grpc-service/internal/service/greeting/greeting.go
@@ -0,0 +1,88 @@
+package greeting
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gardenbed/basil/httpx"
+	"github.com/go-redis/redis/v8"
+
+	"grpc-service/internal/idl/greetingpb"
+)
+
+type (
+	httpClient interface {
+		Do(*http.Request) (*http.Response, error)
+	}
+
+	redisClient interface {
+		Get(context.Context, string) *redis.StringCmd
+		Set(context.Context, string, interface{}, time.Duration) *redis.StatusCmd
+	}
+)
+
+// service implements the greetingpb.GreetingServiceServer interface.
+type service struct {
+	httpClient  httpClient
+	redisClient redisClient
+}
+
+// NewService creates a new service.
+func NewService(httpClient httpClient, redisClient redisClient) (greetingpb.GreetingServiceServer, error) {
+	return &service{
+		httpClient:  httpClient,
+		redisClient: redisClient,
+	}, nil
+}
+
+// Greet implements the GreetingService::Greet endpoint.
+func (s *service) Greet(ctx context.Context, req *greetingpb.GreetRequest) (*greetingpb.GreetResponse, error) {
+	name, err := s.getName(ctx, req.GithubUsername)
+	if err != nil {
+		return nil, err
+	}
+
+	greeting := fmt.Sprintf("Hello, %s!", name)
+	resp := &greetingpb.GreetResponse{
+		Greeting: greeting,
+	}
+
+	return resp, nil
+}
+
+func (s *service) getName(ctx context.Context, username string) (string, error) {
+	name, err := s.redisClient.Get(ctx, username).Result()
+	if err == nil && name != "" {
+		return name, nil
+	}
+
+	url := fmt.Sprintf("https://api.github.com/users/%s", username)
+	req, _ := http.NewRequestWithContext(ctx, "GET", url, nil)
+
+	req.Header.Set("User-Agent", "command-app")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/vnd.github.v3+json")
+
+	resp, err := s.httpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", httpx.NewClientError(resp)
+	}
+
+	user := struct {
+		Name string `json:"name"`
+	}{}
+
+	if err := json.NewDecoder(resp.Body).Decode(&user); err != nil {
+		return "", err
+	}
+
+	return user.Name, nil
+}
---
+++ go/grpc-service/internal/service/greeting/greeting_test.go
@@ -0,0 +1,200 @@
+package greeting
+
+import (
+	"context"
+	"errors"
+	"io"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/stretchr/testify/assert"
+
+	"grpc-service/internal/idl/greetingpb"
+)
+
+func TestNewService(t *testing.T) {
+	tests := []struct {
+		name          string
+		httpClient    *MockHTTPClient
+		redisClient   *MockRedisClient
+		expectedError string
+	}{
+		{
+			name:          "OK",
+			httpClient:    &MockHTTPClient{},
+			redisClient:   &MockRedisClient{},
+			expectedError: "",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			s, err := NewService(tc.httpClient, tc.redisClient)
+
+			if tc.expectedError == "" {
+				assert.NotNil(t, s)
+				assert.NoError(t, err)
+			} else {
+				assert.Nil(t, s)
+				assert.EqualError(t, err, tc.expectedError)
+			}
+		})
+	}
+}
+
+func TestService_Greet(t *testing.T) {
+	req, _ := http.NewRequest("GET", "https://api.github.com/users/octocat", nil)
+
+	tests := []struct {
+		name             string
+		httpClient       *MockHTTPClient
+		redisClient      *MockRedisClient
+		ctx              context.Context
+		request          *greetingpb.GreetRequest
+		expectedResponse *greetingpb.GreetResponse
+		expectedError    string
+	}{
+		{
+			name: "Success_FromCache",
+			redisClient: &MockRedisClient{
+				GetMocks: []GetMock{
+					{OutStringCmd: redis.NewStringResult("Octocat", nil)},
+				},
+			},
+			ctx: context.Background(),
+			request: &greetingpb.GreetRequest{
+				GithubUsername: "octocat",
+			},
+			expectedResponse: &greetingpb.GreetResponse{
+				Greeting: "Hello, Octocat!",
+			},
+			expectedError: "",
+		},
+		{
+			name: "HTTPCallFails",
+			httpClient: &MockHTTPClient{
+				DoMocks: []DoMock{
+					{OutError: errors.New("http error")},
+				},
+			},
+			redisClient: &MockRedisClient{
+				GetMocks: []GetMock{
+					{OutStringCmd: redis.NewStringResult("", errors.New("redis error"))},
+				},
+			},
+			ctx: context.Background(),
+			request: &greetingpb.GreetRequest{
+				GithubUsername: "octocat",
+			},
+			expectedResponse: nil,
+			expectedError:    "http error",
+		},
+		{
+			name: "UnexpectedStatusCode",
+			httpClient: &MockHTTPClient{
+				DoMocks: []DoMock{
+					{
+						OutResponse: &http.Response{
+							Request:    req,
+							StatusCode: 400,
+							Body: io.NopCloser(
+								strings.NewReader(`{ "error": "invalid request" }`),
+							),
+						},
+					},
+				},
+			},
+			redisClient: &MockRedisClient{
+				GetMocks: []GetMock{
+					{OutStringCmd: redis.NewStringResult("", errors.New("redis error"))},
+				},
+			},
+			ctx: context.Background(),
+			request: &greetingpb.GreetRequest{
+				GithubUsername: "octocat",
+			},
+			expectedResponse: nil,
+			expectedError:    "GET /users/octocat 400: invalid request",
+		},
+		{
+			name: "InvalidResponseBody",
+			httpClient: &MockHTTPClient{
+				DoMocks: []DoMock{
+					{
+						OutResponse: &http.Response{
+							Request:    req,
+							StatusCode: 200,
+							Body: io.NopCloser(
+								strings.NewReader(`{`),
+							),
+						},
+					},
+				},
+			},
+			redisClient: &MockRedisClient{
+				GetMocks: []GetMock{
+					{OutStringCmd: redis.NewStringResult("", errors.New("redis error"))},
+				},
+			},
+			ctx: context.Background(),
+			request: &greetingpb.GreetRequest{
+				GithubUsername: "octocat",
+			},
+			expectedResponse: nil,
+			expectedError:    "unexpected EOF",
+		},
+		{
+			name: "Success_FromAPI",
+			httpClient: &MockHTTPClient{
+				DoMocks: []DoMock{
+					{
+						OutResponse: &http.Response{
+							Request:    req,
+							StatusCode: 200,
+							Body: io.NopCloser(
+								strings.NewReader(`{ "id": 1, "login": "octocat", "email": "octocat@example.com", "name": "Octocat" }`),
+							),
+						},
+					},
+				},
+			},
+			redisClient: &MockRedisClient{
+				GetMocks: []GetMock{
+					{OutStringCmd: redis.NewStringResult("", errors.New("redis error"))},
+				},
+				SetMocks: []SetMock{
+					{OutStatusCmd: redis.NewStatusResult("", nil)},
+				},
+			},
+			ctx: context.Background(),
+			request: &greetingpb.GreetRequest{
+				GithubUsername: "octocat",
+			},
+			expectedResponse: &greetingpb.GreetResponse{
+				Greeting: "Hello, Octocat!",
+			},
+			expectedError: "",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			s := &service{
+				httpClient:  tc.httpClient,
+				redisClient: tc.redisClient,
+			}
+
+			response, err := s.Greet(tc.ctx, tc.request)
+
+			if tc.expectedError == "" {
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expectedResponse, response)
+			} else {
+				assert.Nil(t, response)
+				assert.EqualError(t, err, tc.expectedError)
+			}
+		})
+	}
+}
